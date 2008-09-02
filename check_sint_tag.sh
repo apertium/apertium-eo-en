9370c3c13f63968ff0db6fc658685e29617df3d9
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2008-09-02 08:21:45 +0000

    Script to check for <sint> tag, gave following suggestions, which have been followed Remove <s n="sint"> to correct in ____<e><p><l>ĝentila<s_n="adj"/></l><r>correct<s_n="adj"/><s_n="sint"/></r></p></e> Add <s n="sint"> to right in ____<e_r="RL"><p><l>ĝentila<s_n="adj"/></l><r>right<s_n="adj"/></r></p></e> Add <s n="sint"> to lengthy in ____<e_r="RL"><p><l>longa<s_n="adj"/></l><r>lengthy<s_n="adj"/></r></p></e> Add <s n="sint"> to shallow in ____<e_r="RL"><p><l>malprofunda<s_n="adj"/></l><r>shallow<s_n="adj"/></r></p></e> Add <s n="sint"> to floppy in ____<e_r="RL"><p><l>malrigida<s_n="adj"/></l><r>floppy<s_n="adj"/></r></p></e> Add <s n="sint"> to gentle in ____<e_r="RL"><p><l>milda<s_n="adj"/></l><r>gentle<s_n="adj"/></r></p></e> Add <s n="sint"> to holy in ____<e_r="RL"_c="check"><p><l>sankta<s_n="adj"/></l><r>holy<s_n="adj"/></r></p></e> Add <s n="sint"> to weird in ____<e_r="RL"><p><l>stranga<s_n="adj"/></l><r>weird<s_n="adj"/></r></p></e>

---
+++ check_sint_tag.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check for <sint> consistency
+
+lt-expand apertium-eo-en.en.dix | grep '<sint>' | cut -f2 -d':' | cut -f1 -d'<' | uniq > /tmp/sintadj2
+n=0;
+
+for line in `cat apertium-eo-en.eo-en.dix |sed 's/ /_/g'`; 
+do
+  EN=`echo $line | sed 's/<r>/@/g' | cut -f2 -d'@' | cut -f1 -d'<'`; 
+#  n=$((n+1))
+  echo $line | grep '<s_n="adj"/>' > /dev/null; 
+  if [ $? -eq 0 ]; 
+  then 
+#    echo $line;
+    cat /tmp/sintadj2 | grep $EN >/dev/null; 
+    if [ $? -eq 0 ]; 
+    then
+      echo $line | grep '<s_n="sint"/>' > /dev/null; 
+      if [ $? -ne 0 ]; 
+      then
+#        echo Add '<s n="sint">' to $EN  in line $n: $line
+        echo Add '<s n="sint">' to $EN  in $line
+      fi
+    else
+      echo $line | grep '<s_n="sint"/>' > /dev/null; 
+      if [ $? -eq 0 ]; 
+      then
+        echo Remove '<s n="sint">' to $EN  in $line
+      fi
+    fi
+  fi
+done
+
+
