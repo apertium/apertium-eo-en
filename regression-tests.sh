ab7102e31aac525bd8b29bfa1c9b1f6f036cc26e
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2008-10-02 08:12:53 +0000

    Much faster

--- regression-tests.sh
+++ regression-tests.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
- unset LC_ALL
-LIST=`wget -O - -q http://wiki.apertium.org/wiki/English_and_Esperanto/Regression_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
+
+TESTNAME=Regression_tests
 
 cp *.mode modes/
 
-echo "Checking tests that should pass..."
+echo "Downloading test set $TESTNAME"
+
+unset LC_ALL
+mkdir .testcache > /dev/null 2>&1
+cd .testcache
+rm -f test_*
+wget  -N http://wiki.apertium.org/wiki/English_and_Esperanto/$TESTNAME 2>&1 | grep "Remote file is newer" > /dev/null || echo $TESTNAME not changed on webserver
+
+#echo "Preparing (shoulnt be necesaty if $TESTNAME not changed on webserver)"
+
+LIST=`cat $TESTNAME | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
+MODES=
+
 for LINE in $LIST; do
 	dir=`echo $LINE | cut -f2 -d'(' | cut -f1 -d')'`;
 
@@ -15,51 +27,33 @@ for LINE in $LIST; do
 	else 
 		continue;
 	fi
+	MODES=`echo $MODES $mode`
 
 #	echo $LINE;
 	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
 	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g'`;
 
-	TR=`echo $SL | apertium -d . $mode`;
+	echo $SL. >> test_SL_$mode
+	echo $TL. >> test_TL_$mode
 
-	if [[ `echo $TR | tr '[A-Z]' '[a-z]'` != `echo $TL | tr '[A-Z]' '[a-z]'` ]]; then 
-		echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "$TR"\n";
-	fi
+	#TR=`echo $SL | apertium -d .. $mode`;
+	#echo $TR. >> test_TRx_$mode
 
-done
-
-exit 0
-
-echo "Checking tests that are known to fail..."
-
-#LIST=`wget -O - -q http://wiki.apertium.org/wiki/English_and_Esperanto/Outstanding_tests | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
-#LIST=`wget -O - -q http://wiki.apertium.org/wiki/English_and_Esperanto/Outstanding_tests | grep -E '(<li>|<h2>)' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
-LIST=`wget -O - -q http://wiki.apertium.org/wiki/English_and_Esperanto/Outstanding_tests | grep -E '((<li>.*→)|<h2>)' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
-
-cp *.mode modes/
-
-for LINE in $LIST; do
-	dir=`echo $LINE | cut -f2 -d'(' | cut -f1 -d')'`;
-
-	if [ $dir = "en" ]; then
-		mode="en-eo";
-	elif [ $dir = "eo" ]; then
-		mode="eo-en";
-	else 
-		echo ==`echo $LINE | sed 's/^.*_<span_class="mw-headline">//g' | sed 's/<\/span>.*//g' | sed 's/_/ /g'`==
-		continue;
-	fi
-
-#	echo $LINE;
-	SL=`echo $LINE | cut -f2 -d')' | sed 's/<i>//g' | sed 's/<\/i>//g' | cut -f2 -d'*' | sed 's/→/@/g' | cut -f1 -d'@' | sed 's/(note:/@/g' | sed 's/_/ /g'`;
-	TL=`echo $LINE | sed 's/(\w\w)//g' | sed 's/<i>//g' | cut -f2 -d'*' | sed 's/<\/i>_→/@/g' | cut -f2 -d'@' | sed 's/_/ /g'`;
-
-	TR=`echo $SL | apertium -d . $mode`;
-
-	if [[ `echo $TR | tr '[A-Z]' '[a-z]'` != `echo $TL | tr '[A-Z]' '[a-z]'` ]]; then 
-		echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "$TR"\n";
-	else
-		echo -e $mode"\t "$SL"\nWORKS!\t $TL\n";
-	fi
+	#if [[ `echo $TR | tr '[A-Z]' '[a-z]'` != `echo $TL | tr '[A-Z]' '[a-z]'` ]]; then 
+	#	echo -e $mode"\t "$SL"\n\t-$TL\n\t+ "$TR"\n";
+	#fi
 
 done
+
+echo "Translating using Apertium"
+#echo $MODES
+MODES=`for i in $MODES; do echo $i; done | uniq`
+#echo MODES = $MODES
+
+for mode in $MODES; do
+	echo $mode
+ 	cat test_SL_$mode | nl -s : | apertium -d .. $mode > test_TR_$mode
+	cat test_TL_$mode | nl -s : | diff -wi - test_TR_$mode
+done
+
+
