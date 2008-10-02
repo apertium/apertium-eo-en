5af6a6f0a572878338299c3e7cef9e47ea3b5b76
Francis M. Tyers <ftyers@prompsit.com>
2008-10-02 08:19:11 +0000

    change a bit

--- regression-tests.sh
+++ regression-tests.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 TESTNAME=Regression_tests
+TEMPDIR=`mktemp -d`;
+DATADIR=`pwd`;
 
 cp *.mode modes/
 
 echo "Downloading test set $TESTNAME"
 
 unset LC_ALL
-mkdir .testcache > /dev/null 2>&1
-cd .testcache
+cd $TEMPDIR;
 rm -f test_*
 wget  -N http://wiki.apertium.org/wiki/English_and_Esperanto/$TESTNAME 2>&1 | grep "Remote file is newer" > /dev/null || echo $TESTNAME not changed on webserver
 
-#echo "Preparing (shoulnt be necesaty if $TESTNAME not changed on webserver)"
+#echo "Preparing (shouldn't be necessary if $TESTNAME not changed on webserver)"
 
 LIST=`cat $TESTNAME | grep '<li>' | sed 's/<.*li>//g' | sed 's/ /_/g'`;
 MODES=
@@ -36,7 +37,7 @@ for LINE in $LIST; do
 	echo $SL. >> test_SL_$mode
 	echo $TL. >> test_TL_$mode
 
-	#TR=`echo $SL | apertium -d .. $mode`;
+	#TR=`echo $SL | apertium -d $DATADIR $mode`;
 	#echo $TR. >> test_TRx_$mode
 
 	#if [[ `echo $TR | tr '[A-Z]' '[a-z]'` != `echo $TL | tr '[A-Z]' '[a-z]'` ]]; then 
@@ -52,8 +53,8 @@ MODES=`for i in $MODES; do echo $i; done | uniq`
 
 for mode in $MODES; do
 	echo $mode
- 	cat test_SL_$mode | nl -s : | apertium -d .. $mode > test_TR_$mode
-	cat test_TL_$mode | nl -s : | diff -wi - test_TR_$mode
+ 	cat test_SL_$mode | nl -s : | apertium -d $DATADIR $mode > test_TR_$mode
+	cat test_TL_$mode | nl -s : | diff -Naurwi - test_TR_$mode
 done
 
-
+rm -rf $TEMPDIR
