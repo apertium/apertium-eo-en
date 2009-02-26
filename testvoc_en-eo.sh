ad0e9dab78bd8539cf1efcd299aaa78430aa1d48
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-02-26 07:27:09 +0000

    Testvoc-problemoj preskaux cxiuj solvitaj

--- testvoc_en-eo.sh
+++ testvoc_en-eo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-lt-expand apertium-eo-en.en.dix | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee tmp_testvoc1.txt |
+lt-expand apertium-eo-en.en.dix | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer apertium-eo-en.en-eo.t1x  en-eo.t1x.bin  en-eo.autobil.bin |
         apertium-interchunk apertium-eo-en.en-eo.t2x  en-eo.t2x.bin |
         apertium-postchunk apertium-eo-en.en-eo.t3x  en-eo.t3x.bin  | tee tmp_testvoc2.txt |
         lt-proc -d en-eo.autogen.bin > tmp_testvoc3.txt
 
-paste -d _ tmp_testvoc1.txt tmp_testvoc2.txt tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   ---------> /g'
+paste -d _ tmp_testvoc1.txt tmp_testvoc2.txt tmp_testvoc3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   ---------> /g'
 
