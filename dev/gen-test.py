18e4fac7a2c3341d8a505336a0e0d09abddbecb7
Francis M. Tyers <ftyers@prompsit.com>
2009-04-25 09:30:52 +0000

    Add gen test script

---
+++ dev/gen-test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import sys, codecs, copy;
+
+sys.stdin  = codecs.getreader('utf-8')(sys.stdin);
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout);
+sys.stderr = codecs.getwriter('utf-8')(sys.stderr);
+
+c = sys.stdin.read(1);
+
+an = 0;
+buf = '';
+
+while c: #{
+
+	buf = buf + c;
+
+	if c == "'": #{
+		an = an + 1;	
+	#}
+	
+	if c == ' ' or c == '\n': #{
+		an = 0;
+		if buf.count("'") > 1: #{
+			sys.stdout.write(buf.replace("'", "\\'"));
+		else: #{
+			sys.stdout.write(buf);
+		#}
+		buf = '';
+	#}
+
+
+	c = sys.stdin.read(1);
+#}
