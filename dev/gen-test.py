e0816a6ae60a9d1b6d371495d57ef453f28d72f8
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-04-26 05:33:38 +0000

    Genitiva trakto plibonigita

--- dev/gen-test.py
+++ dev/gen-test.py
@@ -12,6 +12,7 @@ c = sys.stdin.read(1);
 
 an = 0;
 buf = '';
+spc = 0;
 
 while c: #{
 
@@ -20,10 +21,18 @@ while c: #{
 	if c == "'": #{
 		an = an + 1;	
 	#}
-	
-	if c == ' ' or c == '\n': #{
+
+	if c == " ": #{
+		spc = spc + 1;
+	#}
+
+	#if c == ' ' or c == '\n' or c == '.' or c == ',' or c == ':' or c == ';': #{
+	if spc>4 or c == '\n' or c == '.' or c == ',' or c == ':' or c == ';': #{
 		an = 0;
+		spc = 0;
 		if buf.count("'") > 1 and buf.count("s'") > 0: #{
+			#TODO sys.stdout.write(buf.replace("'word", "[']word"));
+			#TODO sys.stdout.write(buf.replace("word'", "word[']"));
 			sys.stdout.write(buf.replace("'", "[']"));
 		else: #{
 			sys.stdout.write(buf);
