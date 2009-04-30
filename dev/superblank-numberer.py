0a563faa3ef42258a79d1e06277bdd95c664402e
Francis M. Tyers <ftyers@prompsit.com>
2009-04-30 07:16:46 +0000

    superblank numberer

---
+++ dev/superblank-numberer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+
+import sys;
+
+c = sys.stdin.read(1);
+
+count = 0;
+
+while c: #{
+	if c == '\\': #{
+		c = sys.stdin.read(2);
+		sys.stdout.write('\\');
+		continue;
+	#}
+	if c == '[': #{
+		count = count + 1;
+		while c != ']': #{
+			c = sys.stdin.read(1);
+		#}
+		sys.stdout.write('[' + str(count));
+		continue;
+	#}
+	sys.stdout.write(c);
+
+	c = sys.stdin.read(1);
+#}
