ddd34bad33f429b8d720d84fa740bf45c1640418
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-08-31 18:18:09 +0000

    angla->esperanto testvoc

---
+++ testvoc_eo-en.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ lt-expand apertium-eo-rn.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f1 -d'%' > tmp_testvoc0.txt &
+
+ lt-expand apertium-eo-rn.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
+        apertium-pretransfer|
+        apertium-transfer apertium-eo-en.eo-en.t1x  eo-en.t1x.bin  eo-en.autobil.bin |
+        apertium-interchunk apertium-eo-en.eo-en.t2x  eo-en.t2x.bin |
+        apertium-postchunk apertium-eo-en.eo-en.t3x  eo-en.t3x.bin  | tee tmp_testvoc2.txt |
+        lt-proc -d eo-en.autogen.bin > tmp_testvoc3.txt
+
+paste -d _ tmp_testvoc0.txt tmp_testvoc1.txt tmp_testvoc2.txt tmp_testvoc3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   -----> /g'
+
