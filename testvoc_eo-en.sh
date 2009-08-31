f066f9d0e1bc5c91b918ce600001f8aeddb80373
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-08-31 18:31:13 +0000

    provizore forprenis pp2 kaj pp3

--- testvoc_eo-en.sh
+++ testvoc_eo-en.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
- lt-expand apertium-eo-rn.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f1 -d'%' > tmp_testvoc0.txt &
+ lt-expand apertium-eo-en.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f1 -d'%' > tmp_testvoc0.txt &
 
- lt-expand apertium-eo-rn.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
+ lt-expand apertium-eo-en.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer apertium-eo-en.eo-en.t1x  eo-en.t1x.bin  eo-en.autobil.bin |
         apertium-interchunk apertium-eo-en.eo-en.t2x  eo-en.t2x.bin |
