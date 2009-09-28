4f0bdb9ccf1af53ad93be82d1ee53d9c3047d42c
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-09-28 13:37:11 +0000

    reorder + fix some direction errors

--- testvoc_en-eo.sh
+++ testvoc_en-eo.sh
@@ -7,6 +7,7 @@
         apertium-interchunk apertium-eo-en.en-eo.t2x  en-eo.t2x.bin |
         apertium-postchunk apertium-eo-en.en-eo.t3x  en-eo.t3x.bin  | tee tmp_testvoc2.txt |
         lt-proc -d en-eo.autogen.bin > tmp_testvoc3.txt
+#        lt-proc -d profiler/en-eo.autogen.bin | apertium-dixtools profilecollect dixtools-profiledata.en-eo.eo.txt > tmp_testvoc3.txt
 
 paste -d _ tmp_testvoc1.txt tmp_testvoc2.txt tmp_testvoc3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   ---------> /g'
 
