004f9d5a9f0b9aac68e292d579d531569e4cfa86
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-10-05 15:06:59 +0000

    testvoc

--- testvoc_en-eo.sh
+++ testvoc_en-eo.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+PREF=testcache/testvoc_en-eo
+
 #lt-expand apertium-eo-en.en.dix | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
- lt-expand apertium-eo-en.en.dix | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
+ lt-expand apertium-eo-en.en.dix |grep -v REGEXP | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee ${PREF}1.txt |
         apertium-pretransfer|
         apertium-transfer apertium-eo-en.en-eo.t1x  en-eo.t1x.bin  en-eo.autobil.bin |
         apertium-interchunk apertium-eo-en.en-eo.t2x  en-eo.t2x.bin |
-        apertium-postchunk apertium-eo-en.en-eo.t3x  en-eo.t3x.bin  | tee tmp_testvoc2.txt |
-        lt-proc -d en-eo.autogen.bin > tmp_testvoc3.txt
+        apertium-postchunk apertium-eo-en.en-eo.t3x  en-eo.t3x.bin  | tee  ${PREF}2.txt |
+        lt-proc -d en-eo.autogen.bin >  ${PREF}3.txt
 #        lt-proc -d profiler/en-eo.autogen.bin | apertium-dixtools profilecollect dixtools-profiledata.en-eo.eo.txt > tmp_testvoc3.txt
 
-paste -d _ tmp_testvoc1.txt tmp_testvoc2.txt tmp_testvoc3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   ---------> /g'
+paste -d _  ${PREF}1.txt  ${PREF}2.txt  ${PREF}3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   ---------> /g' > testvoc_en-eo.txt
 
+grep '@' ${PREF}2.txt
+grep '#' ${PREF}3.txt 
--- testvoc_eo-en.sh
+++ testvoc_eo-en.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
- lt-expand apertium-eo-en.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f1 -d'%' > tmp_testvoc0.txt &
+PREF=testcache/testvoc_en-eo
 
- lt-expand apertium-eo-en.eo.dixtmp1 | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee tmp_testvoc1.txt |
+ lt-expand apertium-eo-en.eo.dixtmp1 |grep -v REGEXP | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f1 -d'%' > ${PREF}0.txt &
+
+ lt-expand apertium-eo-en.eo.dixtmp1 |grep -v REGEXP | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^;<sent>$/g' | tee ${PREF}1.txt |
         apertium-pretransfer|
         apertium-transfer apertium-eo-en.eo-en.t1x  eo-en.t1x.bin  eo-en.autobil.bin |
         apertium-interchunk apertium-eo-en.eo-en.t2x  eo-en.t2x.bin |
-        apertium-postchunk apertium-eo-en.eo-en.t3x  eo-en.t3x.bin  | tee tmp_testvoc2.txt |
-        lt-proc -d eo-en.autogen.bin > tmp_testvoc3.txt
+        apertium-postchunk apertium-eo-en.eo-en.t3x  eo-en.t3x.bin  | tee ${PREF}2.txt |
+        lt-proc -d eo-en.autogen.bin > ${PREF}3.txt
 
-paste -d _ tmp_testvoc0.txt tmp_testvoc1.txt tmp_testvoc2.txt tmp_testvoc3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   -----> /g'
+paste -d _  ${PREF}0.txt ${PREF}1.txt ${PREF}2.txt ${PREF}3.txt | sed 's/\^;<sent>\$//g' | sed 's/ \.//g' | sed 's/_/   -----> /g' > testvoc_eo-en.txt
 
+grep '@' ${PREF}2.txt
+grep '#' ${PREF}3.txt 
