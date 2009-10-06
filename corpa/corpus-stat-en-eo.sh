b5484fa102403b1f4a380c1778be8c73af4dd8dd
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2009-10-06 07:08:04 +0000

    Programetoj por doni statistikon pri kovro de tekstaroj

---
+++ corpa/corpus-stat-en-eo.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# http://wiki.apertium.org/wiki/Asturian#Calculating_coverage
+
+
+# Example use:
+# zcat corpa/en.crp.txt.gz | sh corpus-stat.sh
+
+
+#CMD="cat corpa/en.crp.txt"
+#CMD="cut -c9- corpa/en.crp.txt"
+#CMD="cut -c9- en.crp.txt"
+
+bunzip2 -c enwiki.crp.txt.bz2 > /tmp/xx; CMD="cut -c9- /tmp/xx"
+
+F=/tmp/corpus-stat-res-en.txt
+
+# Calculate the number of tokenised words in the corpus:
+# for some reason putting the newline in directly doesn't work, so two seds
+$CMD | apertium-destxt | lt-proc ../en-eo.automorf.bin |apertium-retxt | sed 's/\$[^^]*\^/$^/g' | sed 's/\$\^/$\
+^/g' > $F
+
+NUMWORDS=`cat $F | wc -l`
+echo "Number of tokenised words in the corpus: $NUMWORDS"
+
+
+
+# Calculate the number of words that are not unknown
+
+NUMKNOWNWORDS=`cat $F | grep -v '\*' | wc -l`
+echo "Number of known words in the corpus: $NUMKNOWNWORDS"
+
+
+# Calculate the coverage
+
+COVERAGE=`calc "round($NUMKNOWNWORDS/$NUMWORDS*1000)/10"`
+echo "Coverage: $COVERAGE %"
+
+
+# Show the top-ten unknown words.
+
+echo "Top unknown words in the corpus:"
+cat $F | grep '\*' | sort -f | uniq -c | sort -gr | head -20
+
+rm -f $F
---
+++ corpa/corpus-stat-eo-en.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# http://wiki.apertium.org/wiki/Asturian#Calculating_coverage
+
+
+# Example use:
+# zcat corpa/en.crp.txt.gz | sh corpus-stat.sh
+
+
+#CMD="cut -c9- en.crp.txt"
+#CMD="cat"
+bunzip2 -c eowiki.crp.txt.bz2 > /tmp/xxeo; CMD="cut -c9- /tmp/xxeo"
+
+F=/tmp/corpus-stat-res-eo.txt
+
+# Calculate the number of tokenised words in the corpus:
+# for some reason putting the newline in directly doesn't work, so two seds
+$CMD | apertium-destxt | lt-proc ../eo-en.automorf.bin |apertium-retxt | sed 's/\$[^^]*\^/$^/g' | sed 's/\$\^/$\
+^/g' > $F
+
+NUMWORDS=`cat $F | wc -l`
+echo "Number of tokenised words in the corpus: $NUMWORDS"
+
+
+
+# Calculate the number of words that are not unknown
+
+NUMKNOWNWORDS=`cat $F | grep -v '\*' | wc -l`
+echo "Number of known words in the corpus: $NUMKNOWNWORDS"
+
+
+# Calculate the coverage
+
+COVERAGE=`calc "round($NUMKNOWNWORDS/$NUMWORDS*1000)/10"`
+echo "Coverage: $COVERAGE %"
+
+
+# Show the top-ten unknown words.
+
+echo "Top unknown words in the corpus:"
+cat $F | grep '\*' | sort -f | uniq -c | sort -gr | head -50
+
+rm -f $F
