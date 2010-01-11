b9ea2d442974b3c014c84d4dc33af46cab33bbb4
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2010-01-11 13:17:38 +0000

    metodo enkondukita

--- corpa/esperantowiki-xml2txt.py
+++ corpa/esperantowiki-xml2txt.py
@@ -7,6 +7,68 @@ import re
 import commands;
 
 
+def printline(line):
+	y = 0
+	m = re.findall('\[\[[^]]*\]\]',line)
+	for wlink in m:
+		wlink1 = wlink[2:]
+		wlink2 = wlink1[:-2]
+		if '|' in wlink2:
+			nwords = wlink2.split('|')
+			nword = nwords[-1:][0]
+			line = line.replace(wlink2,nword)
+		if ':' in wlink2:
+			nwords = wlink2.split(':')
+			nword = nwords[-1:][0]
+			line = line.replace(wlink2,nword)
+	line = line.replace('[[','')
+	line = line.replace(']]','')
+	m = re.findall('\{\{[^}]*\}\}',line)
+	for wlink in m:
+		wlink1 = wlink[2:]
+		wlink2 = wlink1[:-2]
+		if '|' in wlink2:
+			nwords = wlink2.split('|')
+			nword = nwords[-1:][0]
+			line = line.replace(wlink2,nword)
+		if ':' in wlink2:
+			nwords = wlink2.split(':')
+			nword = nwords[-1:][0]
+			line = line.replace(wlink2,nword)
+	m = re.findall('&lt[^&]*&gt',line)
+	for wlink in m:
+		line = line.replace(wlink,'')
+	line = line.replace('&amp;nbsp;',' ')
+	line = line.replace('{{','')
+	line = line.replace('}}','')
+	line = line.replace('\'\'\'','')
+	line = line.replace('&quot;','\'\'')
+	line = line.replace('&amp;ndash;','-')
+	line = line.replace('&amp;','&')
+	line = line.replace('      ','')
+	line = line.replace('  ',' ')
+	line = line.replace('</text>','')
+	m = re.search('\<text[^>]*\>',line)
+	if not m == None:
+		oldword = m.group(0)
+		line = line.replace(oldword,'')
+
+	newline = line
+	if not newline == '\n' and not newline[0] == '*' and not newline[0] == '=' and not newline[0]==':' and not newline[0]=='#' and newline[-2:] == '.\n':
+		m = re.findall('...[a-zŝĝĥĵĉŭæøå0-9\'\)»;]\. [A-ZŬŜĜĤĴĈÆØÅ0-9\'«]',newline)
+		for splitw in m:
+			if not 'ĉ. ' in splitw and not 'ekz. ' in splitw and not 'ktp. ' in splitw and not 'i.a. ' in splitw and not 'k.s. ' in splitw and not 'a.K. ' in splitw and not 'p.K. ' in splitw and not 'p. ' in splitw:
+				newsplit = splitw.replace('. ','.\n')
+				newline = newline.replace(splitw,newsplit)
+		y = y+1
+
+		g.write(newline)
+		newline = '';
+#			lingvo = commands.getoutput(text_cat+'-l \''+newline+'\'')
+#			g.write(lingvo+"\n\n")
+#			if 'esperanto' in lingvo:
+#				g.write(newline)
+
 
 
 #inputfile = sys.argv[1]
@@ -27,7 +89,6 @@ inboks2 = 0
 intable = 0
 intable2 = 0
 enkomento = 0
-y = 0
 z = 0
 w = 0
 v = 0
@@ -57,66 +118,7 @@ for line in f:
 		intable2 = 1
 	oldline = line
 	if inprint == 1 and ellasu == 0 and inboks == 0 and inboks2 == 0 and intable == 0 and intable2== 0 and enkomento==0:
-		m = re.findall('\[\[[^]]*\]\]',line)
-		for wlink in m:
-			wlink1 = wlink[2:]
-			wlink2 = wlink1[:-2]
-			if '|' in wlink2:
-				nwords = wlink2.split('|')
-				nword = nwords[-1:][0]
-				line = line.replace(wlink2,nword)
-			if ':' in wlink2:
-				nwords = wlink2.split(':')
-				nword = nwords[-1:][0]
-				line = line.replace(wlink2,nword)
-		line = line.replace('[[','')
-		line = line.replace(']]','')
-		m = re.findall('\{\{[^}]*\}\}',line)
-		for wlink in m:
-			wlink1 = wlink[2:]
-			wlink2 = wlink1[:-2]
-			if '|' in wlink2:
-				nwords = wlink2.split('|')
-				nword = nwords[-1:][0]
-				line = line.replace(wlink2,nword)
-			if ':' in wlink2:
-				nwords = wlink2.split(':')
-				nword = nwords[-1:][0]
-				line = line.replace(wlink2,nword)
-		m = re.findall('&lt[^&]*&gt',line)
-		for wlink in m:
-			line = line.replace(wlink,'')
-		line = line.replace('&amp;nbsp;',' ')
-		line = line.replace('{{','')
-		line = line.replace('}}','')
-		line = line.replace('\'\'\'','')
-		line = line.replace('&quot;','\'\'')
-		line = line.replace('&amp;ndash;','-')
-		line = line.replace('&amp;','&')
-		line = line.replace('      ','')
-		line = line.replace('  ',' ')
-		line = line.replace('</text>','')
-		m = re.search('\<text[^>]*\>',line)
-		if not m == None:
-			oldword = m.group(0)
-			line = line.replace(oldword,'')
-
-		newline = line
-		if not newline == '\n' and not newline[0] == '*' and not newline[0] == '=' and not newline[0]==':' and not newline[0]=='#' and newline[-2:] == '.\n':
-			m = re.findall('...[a-zŝĝĥĵĉŭæøå0-9\'\)»;]\. [A-ZŬŜĜĤĴĈÆØÅ0-9\'«]',newline)
-			for splitw in m:
-				if not 'ĉ. ' in splitw and not 'ekz. ' in splitw and not 'ktp. ' in splitw and not 'i.a. ' in splitw and not 'k.s. ' in splitw and not 'a.K. ' in splitw and not 'p.K. ' in splitw and not 'p. ' in splitw:
-					newsplit = splitw.replace('. ','.\n')
-					newline = newline.replace(splitw,newsplit)
-			y = y+1
-
-			g.write(newline)
-			newline = '';
-#			lingvo = commands.getoutput(text_cat+'-l \''+newline+'\'')
-#			g.write(lingvo+"\n\n")
-#			if 'esperanto' in lingvo:
-#				g.write(newline)
-
+		printline(line);
 
 	if '--&gt;' in oldline:
 		enkomento = 0
