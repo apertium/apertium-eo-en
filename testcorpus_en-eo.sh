d876b28108e2e295304caa1ea9aa7bc636863c0e
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2010-05-05 12:12:59 +0000

    CG enmetita

--- testcorpus_en-eo.sh
+++ testcorpus_en-eo.sh
@@ -14,7 +14,7 @@ fi
 
 #make -s -j 3 && cat $CRP | apertium -d . en-eo > $NOVA || exit
 #make -s -j 3 && cat $CRP | apertium -d . en-eo-bytecode > $NOVA || exit
-cat $CRP | apertium -d . en-eo-bytecode > $NOVA || exit
+cat $CRP | apertium -d . en-eo-bytecode-cg > $NOVA || exit
 echo
 grep '#' $NOVA && echo -e "^ Estis mankoj en la cellingva dix, montrata supre ^\n"
 grep '@' $NOVA && echo -e "^ Estis mankoj en la dulingva dix, montrata supre ^\n"
